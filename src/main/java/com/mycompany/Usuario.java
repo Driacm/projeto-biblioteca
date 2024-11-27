dddff7904e52148846120bf254a5df03f2fcb4ce
ADRIANA MOURA <164930148+Driacm@users.noreply.github.com>
2024-11-27 14:06:14 +0000

    atualizando o arquivo Usuario.java

--- src/main/java/com/mycompany/Usuario.java
+++ src/main/java/com/mycompany/Usuario.java
@@ -2,7 +2,7 @@ package com.mycompany.app;
 
 public class Usuario extends Pessoa {
 
-    public Usuario(String nome, int idade) {
+    public Usuario (String nome, int idade) {
         super(nome, idade);
     }
 }
