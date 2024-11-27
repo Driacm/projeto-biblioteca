7e945cd5fc29b7989730d3563ea06295cffee5ba
ADRIANA MOURA <164930148+Driacm@users.noreply.github.com>
2024-11-27 17:09:26 +0000

    autualiza arquivo PessoaTest.java

---
+++ src/test/java/com/mycompany/PessoaTest.java
@@ -0,0 +1,46 @@
+//instancia de pessoa com um nome
+//getNome :retorna o nome da pessoa (comparar com o nome que passei no setNome)
+
+//setNome :atribuir um valor pra a propriedade nome (comparar com o valor que vem do getNome é o mesmo do setNome)
+//getLivro :atribuir uma lista de livros para uma pessoa (comparar se a lista retornada é a mesma lista que passamos para setLivros)
+//setLivro : atribuir uma lista de livros para uma pessoa (comparar se o valor do getLivros é o mesmo colocado em setLivros)
+package com.mycompany.app;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class PessoaTest {
+
+    @Test
+
+    public void testGetNome() {
+
+        // Cria uma instância da classe Pessoa
+
+        Pessoa pessoa = new Pessoa();
+//atribuir
+}
+}
+
+
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    public AppTest() {
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMore() {
+        assertTrue(true);
+    }
+}
